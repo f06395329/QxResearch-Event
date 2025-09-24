5330d4694a94e462ea9a3d5f9e92a1dd2655185a
f06395329 <f06395329@gmail.com>
2025-09-24 05:57:29 -0400

    refactor(password-protect): add function wrapper and entry-point guard

--- Applications/Password Protect PDF/source-code.py
+++ Applications/Password Protect PDF/source-code.py
@@ -1,10 +1,17 @@
 from PyPDF2 import PdfWriter, PdfReader
 import getpass
-pdfwriter=PdfWriter()
-pdf=PdfReader("1.pdf")
-for page_num in range(len(pdf.pages)):
-  pdfwriter.add_page(pdf.pages[page_num])
-passw=getpass.getpass(prompt='Enter Password: ')
-pdfwriter.encrypt(passw)
-with open('ho.pdf','wb') as f:
-  pdfwriter.write(f)
+
+
+def protect_pdf(input_path='1.pdf', output_path='ho.pdf'):
+    pdfwriter = PdfWriter()
+    pdf = PdfReader(input_path)
+    for page_num in range(len(pdf.pages)):
+        pdfwriter.add_page(pdf.pages[page_num])
+    passw = getpass.getpass(prompt='Enter Password: ')
+    pdfwriter.encrypt(passw)
+    with open(output_path, 'wb') as f:
+        pdfwriter.write(f)
+
+
+if __name__ == '__main__':
+    protect_pdf()
