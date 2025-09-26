5973ba7e13f9c661e7192559a8198c60a87072a7
f06395329 <f06395329@gmail.com>
2025-09-25 22:48:12 -0400

    chore(win-notify): wrap toaster call in main() and add guard

--- Applications/Windows Notification/source-code-custom.py
+++ Applications/Windows Notification/source-code-custom.py
@@ -1,3 +1,10 @@
 import win10toast
-toaster = win10toast.ToastNotifier()
-toaster.show_toast("python","success ! This is working!", duration=10)
+
+
+def main():
+	toaster = win10toast.ToastNotifier()
+	toaster.show_toast("python", "success ! This is working!", duration=10)
+
+
+if __name__ == '__main__':
+	main()
